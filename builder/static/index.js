2aefc63c690022db9ffe598f1724f479a51e387c
yuvipanda <yuvipanda@gmail.com>
2017-04-27 14:56:13 -0700

    Make everything look a lot nicer
    Still needs an actual designer at some point ofc

--- builder/static/index.js
+++ builder/static/index.js
@@ -1,13 +1,16 @@
 $(function(){
 
     $('#build-form').submit(function() {
-        var repo =  ($('#repo').val());
+        var repo =  ($('#repository').val());
+        repo = repo.replace(/^(https?:\/\/)?github.com\//, '');
         var url = '/build/github/' + repo + '/master';
+        $('#log').empty();
         var source = new EventSource(url);
 
         source.addEventListener('message', function(e){
             var data = JSON.parse(e.data);
             $('#log').append($('<li>').text(data.payload));
+            $('#log').animate({scrollTop: $('#log')[0].scrollHeight}, 1000);
             if (data.kind == 'pod.phasechange') {
                 if (data.payload == 'Succeeded' || data.payload == 'Failed') {
                     source.close()
