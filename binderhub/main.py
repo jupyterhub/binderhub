cc9700adc92777803146b89a2187181f0909a4da
yuvipanda <yuvipanda@gmail.com>
2017-05-24 16:48:59 -0700

    Add a stable URL for launching binders!
    - Change the provider interface to do more things separately.
      This lets us re-use it in two places

--- binderhub/main.py
+++ binderhub/main.py
@@ -3,15 +3,20 @@ from tornado import web, gen
 
 class MainHandler(web.RequestHandler):
     def get(self):
-        # Hacks!
-        url = self.get_argument('url', '', True)
-        ref = self.get_argument('ref', 'master', True)
-        filepath = self.get_argument('filepath', '', True).replace('|', '#')
-        submit = self.get_argument('submit', False, True)
+        self.render("index.html", url=None, ref='master', filepath=None, submit=False)
+
+
+class ParameterizedMainHandler(web.RequestHandler):
+    def get(self, provider_prefix, spec):
+        providers = self.settings['repo_providers']
+        if provider_prefix not in self.settings['repo_providers']:
+            raise Exception('wat')
+        provider = self.settings['repo_providers'][provider_prefix](config=self.settings['traitlets_config'], spec=spec)
+
         self.render(
             "index.html",
-            url=url,
-            ref=ref,
-            filepath=filepath,
-            submit=submit
+            url=provider.get_repo_url(),
+            ref=provider.unresolved_ref,
+            filepath=self.get_argument('filepath', None),
+            submit=True
         )
