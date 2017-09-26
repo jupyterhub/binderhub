87b7f12e0550f2cb0753642046bd89576fdec174
Carol Willing <carolcode@willingconsulting.com>
2017-09-26 10:43:30 -0700

    correct line continuation

--- doc/script/flow_diagram.py
+++ doc/script/flow_diagram.py
@@ -105,7 +105,7 @@ def generate_flow_diagram(app):
             x['fontname'] = font_face
     g.node_attr['shape'] = 'box'
  
-    g.layout('dot')
+    g.layout(u'dot')
     g.draw(out_fname, format='svg')
     return g
 
