de0546d15d22a7494a036a40d5efcadee3af202f
yuvipanda <yuvipanda@gmail.com>
2017-10-02 13:36:44 -0700

    Add simple metrics that are exposed via prometheus
    Also fix #144 along the way, since I needed it for collecting
    these metrics anyway.

---
+++ binderhub/metrics.py
@@ -0,0 +1,8 @@
+from .base import BaseHandler
+from prometheus_client import REGISTRY, generate_latest, CONTENT_TYPE_LATEST
+
+
+class MetricsHandler(BaseHandler):
+    async def get(self):
+        self.set_header('Content-Type', CONTENT_TYPE_LATEST)
+        self.write(generate_latest(REGISTRY))
