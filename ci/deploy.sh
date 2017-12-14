0060b9d5f6510374b44e60de96b45ebeaa611229
Min RK <benjaminrk@gmail.com>
2017-12-14 14:37:56 +0100

    go back to publishing the chart in after_success
    fixes for new build.py
    
    - define GIT_SSH_COMMAND
    - add --publish-chart arg
    
    quicker than running deploy as its own job because it doesn't require
    running install and build over again

--- ci/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-openssl aes-256-cbc -K $encrypted_d8355cc3d845_key -iv $encrypted_d8355cc3d845_iv -in travis.enc -out travis -d
-set -x
-chmod 0400 travis
-export GIT_SSH_COMMAND="ssh -i ${PWD}/travis"
-cd helm-chart
-./build.py --commit-range "${TRAVIS_COMMIT_RANGE}" --push --publish-chart
