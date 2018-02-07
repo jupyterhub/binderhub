d9d76132e69d791159d4768dd59c8ed13b2d7067
yuvipanda <yuvipanda@gmail.com>
2018-02-06 16:13:22 -0800

    Make the timeout at which 'too long' message appears
    Current timeout of 30s is too short, since a lot of repositories
    definitely take longer than that

--- binderhub/static/js/index.js
+++ binderhub/static/js/index.js
@@ -265,7 +265,7 @@ $(function(){
                 $('#loader').addClass("paused");
                 $('div#loader-text p').html("Repository " + repo + " is taking a long time to load!<br />See the logs for details.")
                 $('#loader').hover(function() {$('#loader').removeClass("paused")}, function() {$('#loader').addClass("paused")});
-            }, 30000)
+            }, 120000)
         }
 
         $('#build-progress .progress-bar').addClass('hidden');
