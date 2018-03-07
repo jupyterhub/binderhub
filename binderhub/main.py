07f1703822013567b37fa030ddb4b6d2bffb7c5b
Min RK <benjaminrk@gmail.com>
2018-03-07 21:35:32 +0100

    use original escaped spec URL fragment for providers
    instead of tornado’s unquoted value, which turns ‘foo%2Fbar/bat’ into ‘foo/bar/bat’
    which causes problems when the two should be treated differently.
    
    We should avoid this problem when we get to the awesome bar

--- binderhub/main.py
+++ binderhub/main.py
@@ -24,9 +24,15 @@ class MainHandler(BaseHandler):
 class ParameterizedMainHandler(BaseHandler):
     """Main handler that allows different parameter settings"""
 
-    def get(self, provider_prefix, spec):
+    def get(self, provider_prefix, _unescaped_spec):
+        # re-extract spec from request.path
+        # get the original, raw spec, without tornado's unquoting
+        # this is needed because tornado converts 'foo%2Fbar/ref' to 'foo/bar/ref'
+        prefix = '/v2/' + provider_prefix
+        idx = self.request.path.index(prefix)
+        spec = self.request.path[idx + len(prefix) + 1:]
         try:
-            provider = self.get_provider(provider_prefix, spec=spec)
+            self.get_provider(provider_prefix, spec=spec)
         except web.HTTPError:
             raise
         except Exception as e:
@@ -38,7 +44,7 @@ class ParameterizedMainHandler(BaseHandler):
         self.render_template(
             "loading.html",
             base_url=self.settings['base_url'],
-            spec=spec,
+            provider_spec='{}/{}'.format(provider_prefix, spec),
             filepath=self.get_argument('filepath', None),
             urlpath=self.get_argument('urlpath', None),
             submit=True,
