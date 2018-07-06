43664d8623fe4c8aca4bb5c833d04c56a92c9e53
Chris Holdgraf <choldgraf@berkeley.edu>
2018-07-06 15:50:53 -0700

    copybutton extension

--- doc/conf.py
+++ doc/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'autodoc_traits',
+    'sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
