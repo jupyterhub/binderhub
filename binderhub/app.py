ff442da9ffc35e72f7cb85729bb60726ee3cc100
Min RK <benjaminrk@gmail.com>
2018-09-06 16:32:12 +0200

    fixup debug logging

--- binderhub/app.py
+++ binderhub/app.py
@@ -425,7 +425,6 @@ class BinderHub(Application):
         self.log = tornado.log.app_log
 
         self.init_pycurl()
-        self.log.info("Are you there god?")
 
         # initialize kubernetes config
         if self.builder_required:
