e764e69c6c21a41ac164bca72cf588c33af2d03c
Tim Head <betatim@gmail.com>
2019-07-26 14:38:35 +0200

    Update health endpoint test

--- binderhub/tests/test_health.py
+++ binderhub/tests/test_health.py
@@ -1,12 +1,13 @@
 """Test health handler"""
 
-import pytest
-
 from .utils import async_requests
 
 
-#@pytest.mark.remote
 async def test_basic_health(app):
     r = await async_requests.get(app.url + "/health")
-    print(r.json())
+
     assert r.status_code == 200
+    assert r.json() == {
+        "ok": True,
+        "checks": [{"service": "JupyterHub API", "ok": True}],
+    }
