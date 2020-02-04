73ed68113ee7f8c384cdc19bdd9ea588741fb180
Min RK <benjaminrk@gmail.com>
2020-02-04 11:48:31 +0100

    skeleton of v2 handlers

--- binderhub/v2/handlers.py
+++ binderhub/v2/handlers.py
@@ -1,7 +1,7 @@
 from ..base import BaseHandler
 
 
-class V2BuildHandler(BaseHandler):
+class BuildHandler(BaseHandler):
     """POST /v2/build/:provider/:spec triggers a build,
 
     responding with a JWT token containing info
@@ -19,32 +19,46 @@ class V2BuildHandler(BaseHandler):
 
     {
         launch_token: opaque token,
-        events_url: opaque url,
-
+        events_url: opaque url with token,
     }
     """
+
+    path = r"/v2/build/([^/]+)/(.+)"
 
     def post(self, provider, spec):
         # generate username
         # check for image
         # trigger build (if needed)
-        # reply with:
+        # reply with event url, containing jwt token
         pass
 
 
-class V2EventsHandler(BaseHandler):
-    """GET /v2/events/:token logs"""
+class EventsHandler(BaseHandler):
+    """GET /v2/events/:token streams logs
+
+    No transactions should occur as a result of making this request.
+    Reconnects should always be stateless and harmless.
+    """
+
+    path = r"/v2/events/([^/]+)"
 
     def get(self, token):
         # decode_jwt to get build-id, image
         # get/relay build logs
+        # final event sends to the launch url
         pass
 
 
-class V2LaunchHandler(BaseHandler):
+class LaunchHandler(BaseHandler):
+    """POST /v2/launch/:token triggers the actual launch"""
+    path = r"/v2/launch/([^/]+)"
+
     def post(self, token):
         # decode_jwt to get username, servername, image
         # validate???
         # request launch
         # redirect to launch URL
         pass
+
+
+default_handlers = [BuildHandler, EventsHandler, LaunchHandler]
