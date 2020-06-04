d8dbf2d7943414709b32113a0346ea41ec4c9f30
Luciano Bello <luciano.bello@ibm.com>
2020-06-04 10:50:59 -0400

    Add a tip about ad blocker in the loading messages

--- binderhub/static/js/src/loading.js
+++ binderhub/static/js/src/loading.js
@@ -10,6 +10,7 @@ const help_messages = [
     'The tool that powers this page is called <a target="_blank" href="https://binderhub.readthedocs.io">BinderHub</a>. It is an open source tool that you can deploy yourself.',
     'The Binder team has <a target="_blank" href="https://mybinder-sre.readthedocs.io/en/latest/">a site reliability guide</a> that talks about what it is like to run a BinderHub.',
     'You can connect with the Binder community in the <a target="_blank" href="https://discourse.jupyter.org/c/binder">Jupyter community forum</a>.',
+    'Empty log? Notebook not loading? Maybe your ad blocker is interfering. Consider whitelisting mybinder.org in your ad blocker.',
     'Your launch may take longer the first few times a repository is used. This is because our machine needs to create your environment.',
 ]
 
