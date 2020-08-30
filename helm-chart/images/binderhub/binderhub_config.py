b45fc6ac8309aca96e8ce50fe213431c44f14317
Kenan Erdogan <kenanerdogan@gmail.com>
2020-08-30 16:18:36 +0200

    Update helm-chart/images/binderhub/binderhub_config.py
    Co-authored-by: Min RK <benjaminrk@gmail.com>

--- helm-chart/images/binderhub/binderhub_config.py
+++ helm-chart/images/binderhub/binderhub_config.py
@@ -84,9 +84,9 @@ if os.getenv('BUILD_NAMESPACE'):
     c.BinderHub.build_namespace = os.environ['BUILD_NAMESPACE']
 
 if c.BinderHub.auth_enabled:
-    hub_url = get_value('config.BinderHub.hub_url', '')
-    hub_url = urlparse(hub_url)
-    c.HubOAuth.hub_host = '{}://{}'.format(hub_url.scheme, hub_url.netloc)
+    if "hub_url" in config.BinderHub:
+        hub_url = urlparse(config.BinderHub. hub_url)
+        c.HubOAuth.hub_host = '{}://{}'.format(hub_url.scheme, hub_url.netloc)
     if 'base_url' in c.BinderHub:
         c.HubOAuth.base_url = c.BinderHub.base_url
 
