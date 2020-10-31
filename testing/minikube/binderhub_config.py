6a0f116f2907490338f85e658d2bed8c6c32fff4
Erik Sundell <erik@sundellopensource.se>
2020-10-31 17:20:25 +0100

    CI: make dummy secrets apparently dummy

--- testing/minikube/binderhub_config.py
+++ testing/minikube/binderhub_config.py
@@ -7,6 +7,6 @@ except (subprocess.SubprocessError, FileNotFoundError):
     minikube_ip = '192.168.1.100'
 
 c.BinderHub.hub_url = 'http://{}:30123'.format(minikube_ip)
-c.BinderHub.hub_api_token = 'aec7d32df938c0f55e54f09244a350cb29ea612907ed4f07be13d9553d18a8e4'
+c.BinderHub.hub_api_token = 'dummy-binder-secret-token'
 c.BinderHub.use_registry = False
 c.BinderHub.build_namespace = os.environ.get('K8S_NAMESPACE', 'binderhub-test')
