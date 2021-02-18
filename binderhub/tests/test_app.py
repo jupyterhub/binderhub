2f804fdd94b01fbb668edbb78cb3eb1a16bc8c72
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-02-18 14:15:11 +0100

    use the first provider to init the form add default text values, consistent with js, to prevent flickering validate repo_providers to avoid empty dict add repo_providers test

--- binderhub/tests/test_app.py
+++ binderhub/tests/test_app.py
@@ -2,6 +2,13 @@
 
 from subprocess import check_output
 import sys
+import pytest
+
+from traitlets import TraitError
+
+from binderhub.app import BinderHub
+from binderhub.repoproviders import (RepoProvider, GitLabRepoProvider, GitHubRepoProvider)
+
 
 def test_help():
     check_output([sys.executable, '-m', 'binderhub', '-h'])
@@ -9,3 +16,19 @@ def test_help():
 def test_help_all():
     check_output([sys.executable, '-m', 'binderhub', '--help-all'])
 
+def test_repo_providers():
+    b = BinderHub()
+
+    class Provider(RepoProvider):
+        pass
+
+    b.repo_providers = dict(gh=GitHubRepoProvider, gl=GitLabRepoProvider)
+    b.repo_providers = dict(p=Provider)
+
+    class BadProvider():
+        pass
+
+    wrong_repo_providers = [GitHubRepoProvider, {}, 'GitHub', BadProvider]
+    for repo_providers in wrong_repo_providers:
+        with pytest.raises(TraitError):
+            b.repo_providers = repo_providers
