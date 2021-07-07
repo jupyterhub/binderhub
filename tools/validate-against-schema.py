a19ffd88bfc1e94b401033ad46473a7097476914
Erik Sundell <erik.i.sundell@gmail.com>
2021-07-07 04:10:20 +0200

    ci: add copy of schema tools from z2jh

---
+++ tools/validate-against-schema.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import jsonschema
+import os
+import yaml
+
+here_dir = os.path.abspath(os.path.dirname(__file__))
+schema_yaml = os.path.join(
+    here_dir, os.pardir, "helm-chart/binderhub", "schema.yaml"
+)
+values_yaml = os.path.join(
+    here_dir, os.pardir, "helm-chart/binderhub", "values.yaml"
+)
+lint_and_validate_values_yaml = os.path.join(
+    here_dir, "templates", "lint-and-validate-values.yaml"
+)
+
+with open(schema_yaml) as f:
+    schema = yaml.safe_load(f)
+with open(values_yaml) as f:
+    values = yaml.safe_load(f)
+with open(lint_and_validate_values_yaml) as f:
+    lint_and_validate_values = yaml.safe_load(f)
+
+# Validate values.yaml against schema
+print("Validating values.yaml against schema.yaml...")
+jsonschema.validate(values, schema)
+print("OK!")
+print()
+
+# Validate lint-and-validate-values.yaml against schema
+print("Validating lint-and-validate-values.yaml against schema.yaml...")
+jsonschema.validate(lint_and_validate_values, schema)
+print("OK!")
