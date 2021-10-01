f2cd428a521c26f30a472e229d76222b17926a49
Simon Li <orpheus+devel@gmail.com>
2021-10-01 17:47:18 +0100

    Support PEP 518 build

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from jupyter_packaging import wrap_installers, npm_builder
 import os
 
 from setuptools import setup, find_packages
+import sys
+
+# ensure the current directory is on sys.path
+# so versioneer can be imported when pip uses
+# PEP 517/518 build rules.
+# https://github.com/python-versioneer/python-versioneer/issues/193
+sys.path.append(os.path.dirname(__file__))
 
 import versioneer
 
