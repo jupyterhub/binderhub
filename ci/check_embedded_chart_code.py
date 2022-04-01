e3afb290edd652747fa01a46807aea1d168aedb0
Erik Sundell <erik@sundellopensource.se>
2022-04-01 10:49:23 +0200

    pre-commit: add hook to update script generated code in values.yaml

--- ci/check_embedded_chart_code.py
+++ ci/check_embedded_chart_code.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-# FIXME:
-# We currently have some code duplicated in binderhub/binderspawner_mixin.py and
-# helm-chart/binderhub/values.yaml
-# We should remove the embedded code from values.yaml and install the required
-# BinderSpawner code in the JupyterHub container.
+# FIXME: We currently have some code duplicated in
+#        binderhub/binderspawner_mixin.py and helm-chart/binderhub/values.yaml
+#        and we use a pre-commit hook to automatically update the values in
+#        values.yaml whenever binderhub/binderspawner_mixin.py is changed.
+#
+#        We should remove the embedded code from values.yaml and install the required
+#        BinderSpawner code in the JupyterHub container.
+#
 
 # For now just check that the two are in sync
 import argparse
