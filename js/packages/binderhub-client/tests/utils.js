192c332124dfe9134dad9dd1286fe1168ebd8166
YuviPanda <yuvipanda@gmail.com>
2023-10-10 00:34:35 -0700

    Add some more jsdoc types

--- js/packages/binderhub-client/tests/utils.js
+++ js/packages/binderhub-client/tests/utils.js
@@ -1,12 +1,13 @@
 import http from "node:http";
 
 /**
+ * Parse an existing stored EventSource response body into an array of JSON objects
  *
- * @param {string} rawData Full text of the response to parse as EventSource formatted data
+ * @param {string} responseBody Full text of the response to parse as EventSource formatted data
  */
-export function parseEventSource(rawData) {
+export function parseEventSource(responseBody) {
   let messages = [];
-  for (const line of rawData.split("\n")) {
+  for (const line of responseBody.split("\n")) {
     const part = line.slice("data: ".length - 1);
     if (part.trim() !== "") {
       messages.push(JSON.parse(part));
@@ -16,20 +17,26 @@ export function parseEventSource(rawData) {
 }
 
 /**
+ * Temporarily start a HTTP server to serve EventSource resources
  *
- * @param {object} payloads
+ * Returns the serverURL (including the protocol) where the server is listening, as well
+ * as a function that can be used to stop the server.
+ *
+ * @param {object} fakeResponses Mapping of paths to response bodies (in EventSource format)
+ *                               the server should respond with when those paths are requested. All
+ *                               other paths will get a 404 response.
  * @returns {Promise}
  */
-export async function simpleEventSourceServer(payloads) {
+export async function simpleEventSourceServer(fakeResponses) {
   return new Promise((resolve) => {
     const server = http.createServer(async (req, res) => {
-      if (payloads[req.url]) {
+      if (fakeResponses[req.url]) {
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/event-stream");
         // Setup CORS so jest can actually read the data
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.flushHeaders();
-        for (const line of payloads[req.url].split("\n")) {
+        for (const line of fakeResponses[req.url].split("\n")) {
           // Eventsource format requires newlines between each line of message
           res.write(line + "\n\n");
           // Wait at least 1ms between lines, to simulate all the data not arriving at once
