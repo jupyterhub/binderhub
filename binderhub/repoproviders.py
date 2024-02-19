3cff44b2ef7312e08780e5281ca5e9faf73ba072
stevejpurves <steve@curvenote.com>
2024-02-19 21:41:28 +0000

    add missing slug hashing fn

--- binderhub/repoproviders.py
+++ binderhub/repoproviders.py
@@ -16,6 +16,7 @@ import time
 import urllib.parse
 from datetime import datetime, timedelta, timezone
 from urllib.parse import urlparse, urlunparse
+from hashlib import md5
 
 import escapism
 from prometheus_client import Gauge
@@ -262,6 +263,15 @@ class ZenodoProvider(RepoProvider):
     def get_build_slug(self):
         return f"zenodo-{self.record_id}"
 
+
+def get_hashed_slug(url, changes_with_content):
+    """Return a unique slug that is invariant to query parameters in the url"""
+    parsed_url = urlparse(url)
+    stripped_url = urlunparse(
+        (parsed_url.scheme, parsed_url.netloc, parsed_url.path, "", "", "")
+    )
+
+    return "meca-" + md5(f"{stripped_url}-{changes_with_content}".encode()).hexdigest()
 
 class MecaRepoProvider(RepoProvider):
     """BinderHub Provider that can handle the contents of a MECA bundle
