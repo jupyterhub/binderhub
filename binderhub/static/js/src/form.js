01c241ab655daaf34bae1200dded86ff359ab812
stevejpurves <steve@curvenote.com>
2024-02-20 21:03:20 +0000

    revert formatting

--- binderhub/static/js/src/form.js
+++ binderhub/static/js/src/form.js
@@ -1,4 +1,4 @@
-import { getPathType } from './path';
+import { getPathType } from "./path";
 
 /**
  * Parse current values in form and return them with appropriate URL encoding
@@ -11,32 +11,32 @@ import { getPathType } from './path';
  * @returns {}
  */
 export function getBuildFormValues() {
-  const providerPrefix = $('#provider_prefix').val().trim();
-  let repo = $('#repository').val().trim();
-  if (providerPrefix !== 'git') {
-    repo = repo.replace(/^(https?:\/\/)?gist.github.com\//, '');
-    repo = repo.replace(/^(https?:\/\/)?github.com\//, '');
-    repo = repo.replace(/^(https?:\/\/)?gitlab.com\//, '');
+  const providerPrefix = $("#provider_prefix").val().trim();
+  let repo = $("#repository").val().trim();
+  if (providerPrefix !== "git") {
+    repo = repo.replace(/^(https?:\/\/)?gist.github.com\//, "");
+    repo = repo.replace(/^(https?:\/\/)?github.com\//, "");
+    repo = repo.replace(/^(https?:\/\/)?gitlab.com\//, "");
   }
-  // trim trailing or leading '/' on repo
-  repo = repo.replace(/(^\/)|(\/?$)/g, '');
+  // trim trailing or leading "/" on repo
+  repo = repo.replace(/(^\/)|(\/?$)/g, "");
   // git providers encode the URL of the git repository as the repo
   // argument.
-  if (repo.includes('://') || providerPrefix === 'gl') {
+  if (repo.includes("://") || providerPrefix === "gl") {
     repo = encodeURIComponent(repo);
   }
 
-  let ref = $('#ref').val().trim() || $('#ref').attr('placeholder');
+  let ref = $("#ref").val().trim() || $("#ref").attr("placeholder");
   if (
-    providerPrefix === 'zenodo' ||
-    providerPrefix === 'figshare' ||
-    providerPrefix === 'dataverse' ||
-    providerPrefix === 'hydroshare' ||
-    providerPrefix === 'meca'
+    providerPrefix === "zenodo" ||
+    providerPrefix === "figshare" ||
+    providerPrefix === "dataverse" ||
+    providerPrefix === "hydroshare" ||
+    providerPrefix === "meca"
   ) {
-    ref = '';
+    ref = "";
   }
-  const path = $('#filepath').val().trim();
+  const path = $("#filepath").val().trim();
   return {
     providerPrefix: providerPrefix,
     repo: repo,
