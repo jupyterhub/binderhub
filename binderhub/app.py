6fa1f1b8b8d39007a7d83b986d978588e4655302
Min RK <benjaminrk@gmail.com>
2024-05-06 14:07:56 +0200

    set cookie secret unconditionally
    no reason to leave it undefined, even if we don't use it

--- binderhub/app.py
+++ binderhub/app.py
@@ -962,8 +962,7 @@ class BinderHub(Application):
                 "enable_api_only_mode": self.enable_api_only_mode,
             }
         )
-        if self.auth_enabled:
-            self.tornado_settings["cookie_secret"] = os.urandom(32)
+        self.tornado_settings["cookie_secret"] = os.urandom(32)
         if self.cors_allow_origin:
             self.tornado_settings.setdefault("headers", {})[
                 "Access-Control-Allow-Origin"
