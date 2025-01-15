f398f88c41b76aa165f9153e592a3144ae3897fd
Angus Hollands <goosey15@gmail.com>
2025-01-15 09:45:35 +0000

    Update binderhub/base.py
    Co-authored-by: Min RK <benjaminrk@gmail.com>

--- binderhub/base.py
+++ binderhub/base.py
@@ -216,7 +216,7 @@ class VersionHandler(BaseHandler):
     # (e.g. mybinder.org federation when blocking cloud datacenters)
     skip_check_request_ip = True
 
-    def set_default_headers():
+    def set_default_headers(self):
         if "Access-Control-Allow-Origin" not in self.settings.get("headers", {}):
             # allow CORS requests to this endpoint by default
             self.set_header("Access-Control-Allow-Origin", "*")
